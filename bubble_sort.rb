9e70145baeb6b56c27fb016e8a8e860015c159fd
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-23 15:15:17 -0400

    Implement bubble_sort() method

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(unsorted_array)
+  sorted_array = unsorted_array.dup
+
+  for i in 2..unsorted_array.length
+    stop_index = unsorted_array.length - i
+    
+    j = 0
+    while j <= stop_index
+      a = sorted_array[j]
+      b = sorted_array[j + 1]
+
+      if a > b
+        sorted_array[j] = b
+        sorted_array[j + 1] = a
+      end
+
+      j += 1
+    end
+  end
+
+  sorted_array
+end
